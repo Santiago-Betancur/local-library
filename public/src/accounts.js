f52b7a4a0701bc13d78f6191548acedc0290682e
Santiago-Betancur <santibetac@gmail.com>
2023-10-06 17:31:50 -0400

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,63 @@
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  return accounts.find(account => account.id === id);
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+   let sortedAccounts = accounts.sort((a, b) => {
+    const nameA = a.name.last.toUpperCase();
+    const nameB = b.name.last.toUpperCase();
+    if (nameA < nameB){
+      return -1;
+    }
+    if (nameA < nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedAccounts;
+}
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  let fullNames = accounts.map((person) => {
+    const {first, last} = person.name;
+   return `${first} ${last}`;
+});
+ return fullNames;
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
